1ad745544c009bf3acaa21dab73d7b240c10886c
Milici-Ilie <milicicristyan@yahoo.com>
2024-03-16 23:45:09 +0200

    Equality challenge!

--- src/EqualityPrinter.java
+++ src/EqualityPrinter.java
@@ -1,2 +1,19 @@
 public class EqualityPrinter {
+
+    public static void main(String[] args) {
+        printEqual(1, 1, 1);
+    }
+
+    public static void printEqual(int x, int y, int z) {
+
+        if (x < 0 || y < 0 || z < 0) {
+            System.out.println("Invalid Value");
+        } else if (x == z && z == y && x == y) {
+            System.out.println("All numbers are equal");
+        } else if (x != z && z != y && x != y) {
+            System.out.println("All numbers are different");
+        } else {
+            System.out.println("Neither all are equal or different!");
+        }
+    }
 }
